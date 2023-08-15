4e240be3bed21f36c5804781ff3bfbf3d7a32dfa
Pat Lowe <lowe-patrick@att.net>
2023-08-14 21:26:57 -0700

    Added some seeding information for the DB & code to connect to the DB

---
+++ db/seeds.sql
@@ -0,0 +1,27 @@
+INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ("Seth", "Rogan", 3, 2);
+INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ("Paul", "Rudd", 4, 1);
+INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ("James", "Franco", 2, 1);
+INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ("Samwise", "Gamgee", 5, 1);
+INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ("Meriadoc", "Brandybuck", 3, 1);
+INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ("Arwen", "Evenstar", 4, 1);
+INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ("Frodo", "Baggins", 3, 2);
+INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ("Gandalf", "Grey", 1, 2 );
+INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ("Tom", "Bombadil", 1, 2);
+INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ("Saruman", "White", 5, 1);
+
+INSERT INTO department(name) VALUES ("Sales");
+INSERT INTO department(name) VALUES ("Engineering");
+INSERT INTO department(name) VALUES ("Finance");
+INSERT INTO department(name) VALUES ("Marketing");
+INSERT INTO department(name) VALUES ("Human Resources");
+
+INSERT INTO role(title, salary, department_id) VALUES ("Sales Lead", 120000, 1);
+INSERT INTO role(title, salary, department_id) VALUES ("Salesperson", 90000, 1);
+INSERT INTO role(title, salary, department_id) VALUES ("Lead Engineer", 160000, 2);
+INSERT INTO role(title, salary, department_id) VALUES ("Software Engineer", 130000, 2);
+INSERT INTO role(title, salary, department_id) VALUES ("Accountant", 120000, 3);
+INSERT INTO role(title, salary, department_id) VALUES ("Legal Team Lead", 200000, 4);
+INSERT INTO role(title, salary, department_id) VALUES ("Lawyer", 200000, 4);
+INSERT INTO role(title, salary, department_id) VALUES ("HR Lead", 100000, 5);
+INSERT INTO role(title, salary, department_id) VALUES ("HR Representative", 75000, 5);
+
--- server.js
+++ server.js
@@ -1,2 +1,14 @@
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
+const Table = require("console.table");
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "abc123",
+    database: "employee_manager",
+  },
+  console.log("Connected to the employee_manager database.")
+);
